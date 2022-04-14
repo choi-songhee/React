6510b43ff1cec33fe39fb6354f8a560760f080cc
choisonghee <songhee0215@lgupluspartners.co.kr>
2022-04-14 15:16:15 +0900

    app.jsx setting

---
+++ my-app/src/components/contact/Contact.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './contact.scss'
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        Contact
+      </div>
+    );
+  }
+}
+
+
+export default Contact;
